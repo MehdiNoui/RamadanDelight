d995a7cbcd866db776baf9a932c450213d7e47dc
Mehdi <mehdinoui2004@gmail.com>
2025-02-01 03:45:56 +0000

    Added food class

---
+++ src/main/java/net/mehdinoui/ramadandelight/common/item/ModFoods.java
@@ -0,0 +1,12 @@
+package net.mehdinoui.ramadandelight.common.item;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.food.FoodProperties;
+import vectorwing.farmersdelight.common.registry.ModEffects;
+
+import static vectorwing.farmersdelight.common.FoodValues.*;
+
+public class ModFoods {
+    public static final FoodProperties BOUREK = new FoodProperties.Builder().
+            nutrition(6).saturationMod(0.3F).build();
+    }
