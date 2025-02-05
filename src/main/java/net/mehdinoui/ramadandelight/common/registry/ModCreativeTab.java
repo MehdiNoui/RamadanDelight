030814b1e04bb84212fc0b596ef1481e7be89b0b
Mehdi <mehdinoui2004@gmail.com>
2025-02-05 16:35:18 +0000

    organized creative tab

--- src/main/java/net/mehdinoui/ramadandelight/common/registry/ModCreativeTab.java
+++ src/main/java/net/mehdinoui/ramadandelight/common/registry/ModCreativeTab.java
@@ -14,35 +14,45 @@ public class ModCreativeTab {
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RamadanDelight.MOD_ID);
 
     public static final RegistryObject<CreativeModeTab> RamadanDelight_TAB = CREATIVE_MODE_TABS.register("veggiesdelight_tab",
-            () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.BOUREK.get()))
+            () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.PARSLEY.get()))
                     .title(Component.translatable("creativetab.ramadandelight_tab"))
                     .displayItems((pParameters, pOutput) ->{
+                                pOutput.accept(ModBlocks.PARSLEY_BAG.get());
+
+                                pOutput.accept(ModBlocks.WILD_PARSLEY.get());
+
+                                pOutput.accept(ModItems.PARSLEY.get());
+                                pOutput.accept(ModItems.PARSLEY_SEEDS.get());
+
+                                pOutput.accept(ModItems.SMALL_DOUGH.get());
+                                pOutput.accept(ModItems.BOUREK_SHEET.get());
+                                pOutput.accept(ModItems.RAW_BOUREK.get());
+                                pOutput.accept(ModItems.RAW_SAMOSA.get());
+
+                                pOutput.accept(ModItems.FLAT_BREAD.get());
+                                pOutput.accept(ModItems.MANAKISH.get());
+                                pOutput.accept(ModItems.BOUREK.get());
+                                pOutput.accept(ModItems.SAMOSA.get());
+
+                                pOutput.accept(ModItems.RAW_KEBAB.get());
+                                pOutput.accept(ModItems.KEBAB.get());
+
+                                pOutput.accept(ModItems.QUICHE.get());
+                                pOutput.accept(ModItems.QUICHE_SLICE.get());
+
+                                pOutput.accept(ModItems.SAVORY_FILLING.get());
+                                pOutput.accept(ModItems.TABBOULEH.get());
+
                                 pOutput.accept(ModItems.HARIRA.get());
                                 pOutput.accept(ModItems.CHORBA.get());
                                 pOutput.accept(ModItems.HALEEM.get());
-                                pOutput.accept(ModItems.BOUREK_SHEET.get());
-                                pOutput.accept(ModItems.RAW_BOUREK.get());
-                                pOutput.accept(ModItems.SAVORY_FILLING.get());
-                                pOutput.accept(ModItems.BOUREK.get());
-                                pOutput.accept(ModItems.BOUREK_BLOCK.get());
-                                pOutput.accept(ModItems.RAW_SAMOSA.get());
-                                pOutput.accept(ModItems.SAMOSA.get());
-                                pOutput.accept(ModItems.RAW_KEBAB.get());
-                                pOutput.accept(ModItems.KEBAB.get());
-                                pOutput.accept(ModItems.SMALL_DOUGH.get());
-                                pOutput.accept(ModItems.FLAT_BREAD.get());
-                                pOutput.accept(ModItems.MANAKISH.get());
-                                pOutput.accept(ModItems.MAKLOBA_BLOCK.get());
-                                pOutput.accept(ModItems.MAKLOBA.get());
+
                                 pOutput.accept(ModItems.SAYADIEH.get());
                                 pOutput.accept(ModItems.TAGINE.get());
-                                pOutput.accept(ModItems.TABBOULEH.get());
-                                pOutput.accept(ModItems.QUICHE.get());
-                                pOutput.accept(ModItems.QUICHE_SLICE.get());
-                                pOutput.accept(ModItems.PARSLEY.get());
-                                pOutput.accept(ModBlocks.PARSLEY_BAG.get());
-                                pOutput.accept(ModItems.PARSLEY_SEEDS.get());
-                                pOutput.accept(ModBlocks.WILD_PARSLEY.get());
+
+                                pOutput.accept(ModItems.MAKLOBA_BLOCK.get());
+                                pOutput.accept(ModItems.MAKLOBA.get());
+                                pOutput.accept(ModItems.BOUREK_BLOCK.get());
                     }
                     ).build());
 
