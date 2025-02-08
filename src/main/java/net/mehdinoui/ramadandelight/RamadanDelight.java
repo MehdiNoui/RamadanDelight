8a673d07309032df6424ed5c5362458d432da8ec
Mehdi <mehdinoui2004@gmail.com>
2025-02-08 00:25:09 +0000

    Added parsley to the trading pool

--- src/main/java/net/mehdinoui/ramadandelight/RamadanDelight.java
+++ src/main/java/net/mehdinoui/ramadandelight/RamadanDelight.java
@@ -4,19 +4,27 @@ import com.mojang.logging.LogUtils;
 import net.mehdinoui.ramadandelight.common.registry.ModBlocks;
 import net.mehdinoui.ramadandelight.common.registry.ModCreativeTab;
 import net.mehdinoui.ramadandelight.common.registry.ModItems;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.entity.animal.Pig;
+import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.crafting.CompoundIngredient;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraft.world.entity.animal.Chicken;
 import org.slf4j.Logger;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @Mod(RamadanDelight.MOD_ID)
@@ -68,6 +76,22 @@ public class RamadanDelight
         public newSubscriber() {
             MinecraftForge.EVENT_BUS.register(this);
         }
+
+        @SubscribeEvent
+        public static void onVillagerTrades(VillagerTradesEvent event) {
+            if (event.getType() == VillagerProfession.FARMER) {
+                List<VillagerTrades.ItemListing> level1Trades = event.getTrades().get(1);
+
+                level1Trades.add((entity, random) -> new MerchantOffer(
+                        new ItemStack(ModItems.PARSLEY.get(), 26), // Soybean in quantity
+                        new ItemStack(Items.EMERALD, 1), // Resulting Emeralds
+                        12, // Max uses
+                        3, // Villager XP
+                        0.05f // Price multiplier
+                ));
+            }
+        }
     }
+
 
 }
