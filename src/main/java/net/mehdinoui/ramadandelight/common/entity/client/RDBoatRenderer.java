d6ba15129f53e7b6b069a218d8892702f02d5f1f
Mehdi Noui <mehdinoui2004@gmail.com>
2025-08-31 16:03:42 +0100

    Defined a boat renderer

---
+++ src/main/java/net/mehdinoui/ramadandelight/common/entity/client/RDBoatRenderer.java
@@ -0,0 +1,63 @@
+package net.mehdinoui.ramadandelight.common.entity.client;
+
+import com.google.common.collect.ImmutableMap;
+import com.mojang.datafixers.util.Pair;
+import net.mehdinoui.ramadandelight.RamadanDelight;
+import net.mehdinoui.ramadandelight.common.entity.custom.RDBoatChestEntity;
+import net.mehdinoui.ramadandelight.common.entity.custom.RDBoatEntity;
+import net.minecraft.client.model.BoatModel;
+import net.minecraft.client.model.ChestBoatModel;
+import net.minecraft.client.model.ListModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.entity.BoatRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.vehicle.Boat;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import javax.swing.*;
+
+public class RDBoatRenderer extends BoatRenderer {
+    private final Map<RDBoatEntity.Type, Pair<ResourceLocation, ListModel<Boat>>> boatResources;
+
+    public RDBoatRenderer(EntityRendererProvider.Context pContext, boolean pChestBoat) {
+        super(pContext, pChestBoat);
+        this.boatResources = Stream.of(RDBoatEntity.Type.values()).collect(ImmutableMap.toImmutableMap(type -> type,
+                type -> Pair.of(new ResourceLocation(RamadanDelight.MOD_ID, getTextureLocation(type, pChestBoat)), this.createBoatModel(pContext, type, pChestBoat))));
+    }
+
+    private static String getTextureLocation(RDBoatEntity.Type pType, boolean pChestBoat) {
+        return pChestBoat ? "textures/entity/chest_boat/" + pType.getName() + ".png" : "textures/entity/boat/" + pType.getName() + ".png";
+    }
+
+    private ListModel<Boat> createBoatModel(EntityRendererProvider.Context pContext, RDBoatEntity.Type pType, boolean pChestBoat) {
+        ModelLayerLocation modellayerlocation = pChestBoat ? RDBoatRenderer.createChestBoatModelName(pType) : RDBoatRenderer.createBoatModelName(pType);
+        ModelPart modelpart = pContext.bakeLayer(modellayerlocation);
+        return pChestBoat ? new ChestBoatModel(modelpart) : new BoatModel(modelpart);
+    }
+
+    public static ModelLayerLocation createBoatModelName(RDBoatEntity.Type pType) {
+        return createLocation("boat/" + pType.getName(), "main");
+    }
+
+    public static ModelLayerLocation createChestBoatModelName(RDBoatEntity.Type pType) {
+        return createLocation("chest_boat/" + pType.getName(), "main");
+    }
+
+    private static ModelLayerLocation createLocation(String pPath, String pModel) {
+        return new ModelLayerLocation(new ResourceLocation(RamadanDelight.MOD_ID, pPath), pModel);
+    }
+
+    public Pair<ResourceLocation, ListModel<Boat>> getModelWithLocation(Boat boat) {
+        if(boat instanceof RDBoatEntity modBoat) {
+            return this.boatResources.get(modBoat.getModVariant());
+        } else if(boat instanceof RDBoatChestEntity modChestBoatEntity) {
+            return this.boatResources.get(modChestBoatEntity.getModVariant());
+        } else {
+            return null;
+        }
+    }
+}
