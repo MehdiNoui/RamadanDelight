d5a961551351650322df7a51a2728b28d99268db
MehdiNoui <mehdinoui2004@gmail.com>
2026-01-16 21:05:16 +0100

    rename to ModConfiguration and added World Gen config keys

--- src/main/java/net/mehdinoui/ramadandelight/ModConfiguration.java
+++ src/main/java/net/mehdinoui/ramadandelight/ModConfiguration.java
@@ -2,7 +2,7 @@ package net.mehdinoui.ramadandelight;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
-public class Configuration {
+public class ModConfiguration {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec CONFIG;
 
@@ -11,6 +11,12 @@ public class Configuration {
     public static final ForgeConfigSpec.BooleanValue ENABLE_FARMER_VILLAGER_TRADES;
     public static final ForgeConfigSpec.BooleanValue ENABLE_MASON_VILLAGER_TRADES;
     public static final ForgeConfigSpec.BooleanValue ENABLE_WANDERING_TRADER_SELLS;
+
+    // --- World Generation ---
+    public static final ForgeConfigSpec.IntValue CHANCE_DATE_PALM_TREE;
+    public static final ForgeConfigSpec.IntValue COUNT_DATE_PALM_TREE;
+    public static final ForgeConfigSpec.IntValue CHANCE_WILD_CHICKPEA;
+    public static final ForgeConfigSpec.IntValue CHANCE_WILD_PARSLEY;
 
     public static final ForgeConfigSpec.BooleanValue GENERATE_VILLAGE_STRUCTURES;
 
@@ -39,6 +45,25 @@ public class Configuration {
         GENERATE_VILLAGE_STRUCTURES = BUILDER
                 .comment("Enable Ramadan Delight to add Palm Trees to Desert Villages")
                 .define("enableVillageStructures", true);
+
+        BUILDER.push("Worldgen Settings");
+
+        CHANCE_DATE_PALM_TREE = BUILDER
+                .comment("Chance of Date Palm Tree generating in the world. (0 to disable, higher = rarer)")
+                .defineInRange("chanceDatePalm", 100, 0, 512);
+        COUNT_DATE_PALM_TREE = BUILDER
+                .comment("Count of Date Palm Trees generating per chunk (0 to disable, higher = rarer)")
+                .defineInRange("countDatePalm", 1, 0, 16);
+
+        CHANCE_WILD_CHICKPEA = BUILDER
+                .comment("Chance of Wild Chickpea in the world. (0 to disable, higher = rarer)")
+                .defineInRange("chanceChickpea", 110, 0, 512);
+        CHANCE_WILD_PARSLEY = BUILDER
+                .comment("Chance of Parsley generating in the world. (0 to disable, higher = rarer)")
+                .defineInRange("chanceParsley", 130, 0, 512);
+
+        BUILDER.pop();
+        BUILDER.pop();
 
         CONFIG = BUILDER.build();
     }
