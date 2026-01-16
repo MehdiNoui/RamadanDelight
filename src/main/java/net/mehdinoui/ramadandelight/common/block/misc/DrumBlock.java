151ecddca7c6a51f6ca7bebf332d58b4389c2f2c
MehdiNoui <mehdinoui2004@gmail.com>
2026-01-16 21:49:25 +0100

    Fix drum subtitles and enhance interactivity with random pitch and particles - Fixed sound registry and subtitles. - Added random pitch variation (0.8 - 1.2) to DrumBlock on interaction. - Added NOTE particles on the client side when the drum is played.

--- src/main/java/net/mehdinoui/ramadandelight/common/block/misc/DrumBlock.java
+++ src/main/java/net/mehdinoui/ramadandelight/common/block/misc/DrumBlock.java
@@ -1,7 +1,8 @@
 package net.mehdinoui.ramadandelight.common.block.misc;
 
-import net.mehdinoui.ramadandelight.common.registry.ModSounds;
+import net.mehdinoui.ramadandelight.common.registry.ModSoundEvents;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
@@ -28,11 +29,16 @@ public class DrumBlock extends Block {
     }
 
     @Override
-    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        if (!world.isClientSide) {
-            SoundEvent drumSound = ModSounds.Drum.get();
-            world.playSound(null, pos, drumSound, SoundSource.BLOCKS, 1.0F, 1.0F);
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (!level.isClientSide) {
+            SoundEvent drumSound = ModSoundEvents.DRUM_BEAT.get();
+            float randomPitch = 0.8F + level.random.nextFloat() * 0.4F;
+            level.playSound(null, pos, drumSound, SoundSource.BLOCKS, 1.0F, randomPitch);
             return InteractionResult.SUCCESS;
+        } else {
+            level.addParticle(ParticleTypes.NOTE,
+                    pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+                    (double) level.random.nextInt(24) / 24.0D, 0.0D, 0.0D);
         }
         return InteractionResult.CONSUME;
     }
