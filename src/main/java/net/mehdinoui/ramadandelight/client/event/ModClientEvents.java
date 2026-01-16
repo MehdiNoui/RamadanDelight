b55bce0bf97446bd69c92f6ac6f44e83503c654b
MehdiNoui <mehdinoui2004@gmail.com>
2026-01-16 01:43:07 +0100

    add custom tooltip for date syrup - Since it behaves like milk, some players may not have an idea about it

--- src/main/java/net/mehdinoui/ramadandelight/client/event/ModClientEvents.java
+++ src/main/java/net/mehdinoui/ramadandelight/client/event/ModClientEvents.java
@@ -4,22 +4,48 @@ import net.mehdinoui.ramadandelight.RamadanDelight;
 import net.mehdinoui.ramadandelight.client.entity.ModModelLayers;
 import net.mehdinoui.ramadandelight.client.entity.RDBoatRenderer;
 import net.mehdinoui.ramadandelight.common.registry.ModEntities;
+import net.mehdinoui.ramadandelight.common.registry.ModItems;
 import net.mehdinoui.ramadandelight.common.util.RDWoodTypes;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.model.BoatModel;
 import net.minecraft.client.model.ChestBoatModel;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+
+import java.util.List;
 
 
 @EventBusSubscriber(modid = RamadanDelight.MOD_ID, value = Dist.CLIENT)
 public class ModClientEvents {
+    @SubscribeEvent
+    public static void registerToolTips(ItemTooltipEvent event) {
+        ItemStack stack = event.getItemStack();
+        List<Component> tooltip = event.getToolTip();
+        if (stack.is(ModItems.DATE_SYRUP)){
+            Component header = Component.translatable("potion.whenDrank").withStyle(ChatFormatting.DARK_PURPLE);
+            Component effectDesc = Component.translatable("tooltip.ramadandelight.date_syrup_desc")
+                    .withStyle(ChatFormatting.BLUE);
+
+             boolean headerExists = tooltip.stream()
+                    .anyMatch(c -> c.getString().equals(header.getString()));
+
+            if (!headerExists) {
+                tooltip.add(Component.empty());
+                tooltip.add(header);
+            }
+            tooltip.add(effectDesc);
+        }
+    }
     @SubscribeEvent
     public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(ModModelLayers.PALM_BOAT_LAYER, BoatModel::createBodyModel);
