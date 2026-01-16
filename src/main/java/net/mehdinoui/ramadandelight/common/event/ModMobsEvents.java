4240bf278dd73722b18042320dd42a6108d759de
MehdiNoui <mehdinoui2004@gmail.com>
2026-01-16 15:47:02 +0100

    add food points to villager wanted Items

--- src/main/java/net/mehdinoui/ramadandelight/common/event/ModMobsEvents.java
+++ src/main/java/net/mehdinoui/ramadandelight/common/event/ModMobsEvents.java
@@ -1,5 +1,6 @@
 package net.mehdinoui.ramadandelight.common.event;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import net.mehdinoui.ramadandelight.RamadanDelight;
@@ -10,6 +11,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
+import java.util.HashMap;
 import java.util.Set;
 
 @EventBusSubscriber(modid = RamadanDelight.MOD_ID)
@@ -23,11 +25,21 @@ public class ModMobsEvents {
                 // Crops
                 ModItems.CHICKPEA.get(),
                 ModItems.PARSLEY.get(),
-                ModItems.PARSLEY_SEEDS.get(),
                 // Fruits
                 ModItems.DATE.get());
 
         newWantedItems.addAll(Villager.WANTED_ITEMS);
         Villager.WANTED_ITEMS = ImmutableSet.copyOf(newWantedItems);
+
+        HashMap<Item, Integer> newFoodPoints = new HashMap<>();
+
+        newFoodPoints.put(ModItems.FLAT_BREAD.get(), 4);
+
+        newFoodPoints.put(ModItems.CHICKPEA.get(), 1);
+        newFoodPoints.put(ModItems.PARSLEY.get(), 1);
+        newFoodPoints.put(ModItems.DATE.get(), 1);
+        newFoodPoints.putAll(Villager.FOOD_POINTS);
+
+        Villager.FOOD_POINTS = ImmutableMap.copyOf(newFoodPoints);
     }
 }
